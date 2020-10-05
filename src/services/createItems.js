9b8f4fac46c6b63a11ffb238e77934d59953c442
Daniel <daniel.fernandez@eotica.com.br>
2020-10-05 19:30:23 -0300

    fix create folder method response

--- src/services/createItems.js
+++ src/services/createItems.js
@@ -15,27 +15,35 @@ router.post('/key', multer(multerConfig).single('file'), async (request, respons
 
 router.post('/folder', async (request, response) => {
   const { folder } = request.query
-  const result = {}
-  let status = 201
+
   s3.headObject({
     Bucket: process.env.AWS_BUCKET,
     Key: `${folder}/`
     }, (err, data) => {
+      if (data){
+        response.status(400).json({
+          message: `This folder already exists with ETag: ${data.ETag}.`
+        })
+      }
       if (err && err.code === 'NotFound'){
         s3.putObject({
           Bucket: process.env.AWS_BUCKET,
           Key: `${folder}/`
-        }, (err2, data2) => {
-          if (err2){
-            result.message = err2.message
-          } else {
-            result.message = 'Folder created successfully.'
+        }, (err, data) => {
+          if (err) {
+            response.status(404).json({
+              message: `Error found when creating folder.`
+            })
+          }
+          if (data) {
+            response.status(201).json({
+              message: `Folder created successfully with ETag: ${data}.`
+            })
           }
         })
       }
-    }
-  )
-  response.status(status).json(result)
-})
+    })
+
+  })
 
 module.exports = router
